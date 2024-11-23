f8bd37b4a4dde97537f2550557bec4a7703468ee
seema <seemamodi0901@gmail.com>
2024-11-24 01:59:19 +0530

    fixes

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import rewriteAll from 'vite-plugin-rewrite-all';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), rewriteAll()],
   css: {
     preprocessorOptions: {
       scss: {

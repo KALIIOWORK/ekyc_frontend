14b463556d8092d3ccab148d8112ca37574ad3ff
seema <seemamodi0901@gmail.com>
2024-11-23 18:31:18 +0530

    new fixes

--- src/components/VideoCall/Basics.jsx
+++ src/components/VideoCall/Basics.jsx
@@ -162,12 +162,12 @@ export const Basics = ({ eKYCId }) => {
                                 <span>Agent is Offline</span>
                             </div>
                         )}
-                        <button
+                        {/* <button
                             onClick={handleEndCall}
                             className="text-gray-700 bg-gray-200 rounded-lg shadow-md font-semibold py-2 px-4 lg:px-6 hover:bg-red-600 hover:text-white transition duration-200"
                         >
                             End Call
-                        </button>
+                        </button> */}
                     </div>
                 </div>
             )}
